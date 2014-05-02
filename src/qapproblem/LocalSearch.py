a8640cb485901cb1b75adfbd0ecdfaac72e9a294
Alejandro Alcalde <algui91@gmail.com>
2014-05-02 10:12:00 +0200

    Local Search fixes

--- src/qapproblem/LocalSearch.py
+++ src/qapproblem/LocalSearch.py
@@ -21,32 +21,31 @@ class LocalSearch(Heuristic):
         '''
         super(LocalSearch, self).__init__(f_name, seed)
         self._DLB = [0] * self._data.tam
-        
+
         def timewrapper():
             return self._find_solution()
         self.exec_time =  Timer(timewrapper).timeit(number=1)
-        
+
     def _find_solution(self):
         num_eval = 0
-        done = False
-
+        max_evals = 10000
+        improve_flag = True
         # Falta el bucle externo hasta no mejora o num_eval <= 10000
-        while num_eval < 10000 and not done:
-            for i in xrange(self._tam):
+        while num_eval < max_evals and improve_flag:
+            improve_flag = False
+            i = 0
+            while i < self._tam and not improve_flag:
                 if self._DLB[i] == 0:
-                    improve_flag = False
                     j = 0
-                    while j < self._tam and not improve_flag and num_eval < 10000:
-                        neighbor = self.swap(i, j)
+                    while j < self._tam and not improve_flag: # and num_eval < max_evals:
                         improvement = self.deltaC(i, j)
                         num_eval += 1
                         if (improvement < 0):
                             self.cost += improvement
-                            self.S = neighbor
+                            self.S = self.swap(i, j)
                             self._DLB[i] = self._DLB[j] = 0
                             improve_flag = True
                         j += 1
                     if not improve_flag:
                         self._DLB[i] = 1
-                print num_eval
-            done = True
+                i += 1
--- src/run_in_all.sh
+++ src/run_in_all.sh
@@ -7,7 +7,8 @@ s=3264321546 # 8 desv sa
 
 s=$(od -vAn -N4 -tu4 < /dev/urandom)
 
-s=3264321546 # 8 desv sa
+#s=3264321546 # 8 desv sa
+s=2149199006
 ficheros="els19.dat chr20a.dat chr25a.dat nug25.dat bur26a.dat bur26b.dat tai30a.dat tai30b.dat esc32a.dat kra32.dat tai35a.dat tai35b.dat tho40.dat tai40a.dat sko42.dat sko49.dat tai50a.dat tai50b.dat tai60a.dat lipa90a.dat"
 
 echo -e "seed=$s\n\n\n"
@@ -24,7 +25,7 @@ do
     python QAP.py -d "data/"$i -a sa -s $s
 done
 echo -e "\n\n\n\n\n"
-for i in $ficheros
-do
-    python QAP.py -d "data/"$i -a greedy -s $s
-done
+#for i in $ficheros
+#do
+#    python QAP.py -d "data/"$i -a greedy -s $s
+#done
