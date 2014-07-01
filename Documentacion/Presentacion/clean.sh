201db06561bceb49af65be7350319aaf5787c97f
Alejandro Alcalde <algui91@gmail.com>
2014-07-01 14:30:09 +0200

    Added Latex to gitignore

--- Documentacion/Presentacion/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm *.{aux,log,pyg,toc,out,dvi,bbl,bcf,blg,xml}
--- Documentacion/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-xelatex -shell-escape Main
-makeindex main.idx -s StyleInd.ist
-biber Main
-xelatex -shell-escape Main
