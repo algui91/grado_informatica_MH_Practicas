7bdaa4c66f54ec977c199d2ad7186aac59ffe09b
Alejandro Alcalde <algui91@gmail.com>
2014-07-13 19:56:40 +0200

    Implemented Swap Path Crossover to Genetic hibrid

--- src/qapproblem/CompeticionPoblacional.py
+++ src/qapproblem/CompeticionPoblacional.py
@@ -5,7 +5,8 @@ Created on June 27, 2014
 
 Licensed under GPLv3
 '''
-import time
+import heapq
+from operator import itemgetter
 from timeit import Timer
 
 from qapproblem.MA_10_1_PMX import MA_10_1_PMX
@@ -30,12 +31,10 @@ class CompeticionPoblacional(MA_10_1_PMX):
         '''
         super(CompeticionPoblacional, self).__init__(f_name, seed)
         
-        
-        start = time.time()        
+             
         def timewrapper():
             return self._find_solution()
         self.exec_time = Timer(timewrapper).timeit(number=1)
-        print time.time() - start
     
     def _find_solution(self):
         
@@ -70,3 +69,51 @@ class CompeticionPoblacional(MA_10_1_PMX):
         
         self.S = self.best_guy[2]
         self.cost = self.best_current_cost  
+        
+    def cross(self):
+        '''
+        We need to cross `how_many_cross` individuals 
+        with the SPX operator (Swap Path Crossover)
+        '''
+        
+        for i in xrange(0, self.how_many_cross, 2):
+            parent1 = self.current_population[i][2]
+            parent2 = self.current_population[i + 1][2]
+            
+            child1, child2 = self.get_two_childs_SPX(parent1, parent2)
+                
+            self.current_population[i][2] = list(child1)
+            self.current_population[i][0] = 1
+            self.current_population[i + 1][2] = list(child2)
+            self.current_population[i + 1][0] = 1
+            
+    def get_two_childs_SPX(self, p1, p2):
+        '''
+        Get two children based on the SPX operator (Swap Path Crossover)
+        '''
+        #cut1 = randint(0, self.n - 1)
+        
+        childs = []
+        append = childs.append
+        C = self.C
+        
+        child1 = list(p1)
+        
+        for i in xrange(self.n):
+            if p1[i] != p2[i]:
+                index1 = p1.index(p2[i])
+                child1[i], child1[index1] = child1[index1], child1[i]
+                [append([C(child1), list(child1)])]
+        
+        # Remove repeated results
+        for c in childs:
+            c[1] = tuple(c[1])
+        childs = map(tuple, childs)
+        childs = set(childs)
+        
+        best_guys = heapq.nsmallest(2, childs, key=itemgetter(0))
+        
+        if best_guys == []:
+            return p1, p2
+        return best_guys[0][1], best_guys[1][1]
+        
